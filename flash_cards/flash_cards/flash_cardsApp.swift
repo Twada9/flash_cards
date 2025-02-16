6c1d1470878bd39966e07839ee794fea15d1d2b3
Twada9 <wada9ta@gmail.com>
2025-02-16 11:30:20 +0900

    [add]プロジェクト追加

---
+++ flash_cards/flash_cards/flash_cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flash_cardsApp.swift
+//  flash_cards
+//
+//  Created by wada on 2025/02/16.
+//
+
+import SwiftUI
+
+@main
+struct flash_cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
